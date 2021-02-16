b4ff271adc664a88452ffd01e1985c2960b9cf61
CodeOne45 <malikaman1999@gmail.com>
2021-02-16 15:29:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.shoppingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/Register.java
@@ -0,0 +1,128 @@
+package com.example.shoppingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    EditText uEmail, uUsername, uPassword;
+    Button uRegisterBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fStrore;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Fill the User information
+        uEmail = findViewById(R.id.input_UEmailR);
+        uUsername = findViewById(R.id.input_UnameR);
+        uPassword = findViewById(R.id.input_UPwdR);
+        uRegisterBtn = findViewById(R.id.btn_Register);
+        progressBar = findViewById(R.id.progressBar);
+
+        // Connect to DB --> Fire base
+        fAuth = FirebaseAuth.getInstance();
+        fStrore = FirebaseFirestore.getInstance();
+
+        // User already exist
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        // Register User
+        uRegisterBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final String email = uEmail.getText().toString().trim();
+                String pwd = uPassword.getText().toString().trim();
+                final String userName = uUsername.getText().toString();
+
+
+                // Check email & password
+                if(TextUtils.isEmpty(email)){
+                    uEmail.setError("Email is required !");
+                    return ;
+                }
+                if(TextUtils.isEmpty((pwd))){
+                    uPassword.setError("Password is required !");
+                    return;
+                }
+                if(pwd.length() < 6){
+                    uPassword.setError("Password must contain 6 or more characters !");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Register the user in to fire base
+                fAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+
+
+                            // Create data
+                            final Map<String,Object> user = new HashMap<>();
+                            user.put("uName", userName);
+                            user.put("uEmail", email);
+
+                            // Store data to DB
+                            fStrore.collection("users")
+                                    .add(user)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error adding document", e);
+                                        }
+                                    });
+
+                            // Go to login activity
+                            startActivity(new Intent(getApplicationContext(), Login.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error !" + task.getException(),Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
